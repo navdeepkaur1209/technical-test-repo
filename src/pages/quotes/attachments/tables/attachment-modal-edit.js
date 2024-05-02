1d9de76fab76732b19555f62a381e7c62994c8ec
Rupi <development@infusion121.com>
2024-05-03 09:53:52 +1000

    edit page update

--- src/pages/quotes/attachments/tables/attachment-modal-edit.js
+++ src/pages/quotes/attachments/tables/attachment-modal-edit.js
@@ -1,14 +1,10 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
-import { isEmpty } from 'lodash';
+import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { CloudUploadOutlined } from '@ant-design/icons';
-import { uploadFileToS3 } from 'utils/helpers';
-import { useDispatch } from 'store';
-import { openSnackbar } from 'store/reducers/snackbar';
 
 // Material
 import TextField from '@mui/material/TextField';
-import { Grid, Button, Alert } from '@mui/material';
+import { Grid, Button } from '@mui/material';
 
 // Formik
 import { Formik, Form } from 'formik';
@@ -17,12 +13,6 @@ import * as yup from 'yup';
 // GraphQL.
 import useGraphQLQuery from 'hooks/useGraphQLQuery';
 import GetAttachment from 'graphql/quotes/GetAttachment';
-import useGraphQLMutation from 'hooks/useGraphQLMutation';
-import useGraphQLLazyQuery from 'hooks/useGraphQLLazyQuery';
-//import ListFiles from 'graphql/quotes/ListFiles';
-import UpdateAttachment from 'graphql/quotes/UpdateAttachment';
-// import DeleteFiles from 'graphql/quotes/DeleteFiles';
-import GetAttachmentUploadPermission from 'graphql/quotes/GetAttachmentUploadPermission';
 
 /* Static methods and values */
 const formikValidationSchema = yup.object().shape({
@@ -36,37 +26,8 @@ const formikInitialValues = {
 };
 
 const AttachmentModalEdit = ({ quoteId, row, callback }) => {
-  const [uploading, setUploading] = useState(false);
   const formikRef = useRef();
-  const dispatch = useDispatch();
-  const [files, setFiles] = useState();
-  const [fileType, setFileType] = useState();
-  const [mutationStatus, setMutationStatus] = useState('');
-  const [mutationMessage, setMutationMessage] = useState('');
-
-  /* GraphQL: GetAttachmentUploadPermission */
-  const [doGetAttachmentUploadPermission, { data: dataGetAttachmentUploadPermission }] = useGraphQLLazyQuery(
-    GetAttachmentUploadPermission,
-    {
-      variables: { id: quoteId, attachmentId: row['AttachmentId'] }
-    }
-  );
-
-  const doPostMutation = useCallback(() => {
-    callback(true);
-  }, [callback]);
-
-  useEffect(() => {
-    if (dataGetAttachmentUploadPermission && dataGetAttachmentUploadPermission.GetAttachmentUploadPermission) {
-      const { Url, Fields } = dataGetAttachmentUploadPermission.GetAttachmentUploadPermission;
-      const files = document.querySelector('#quote-attachment-file');
-      const uploads = [];
-      for (const file of files.files) {
-        uploads.push(uploadFileToS3(Url, JSON.parse(Fields), file));
-      }
-      doPostMutation(true);
-    }
-  }, [dataGetAttachmentUploadPermission, quoteId, dispatch, doPostMutation]);
+  const [files, setFiles] = useState('');
 
   /* GraphQL: GetAttachment */
   const { data: dataGetAttachment } = useGraphQLQuery(GetAttachment, { variables: { id: quoteId, attachmentId: row['AttachmentId'] } });
@@ -82,65 +43,6 @@ const AttachmentModalEdit = ({ quoteId, row, callback }) => {
     }
   }, [dataGetAttachment]);
 
-  /* GraphQL: UpdateAttachment */
-  const [mutationUpdateAttachment, { data: resultUpdateAttachment, reset: resetUpdateAttachment }] = useGraphQLMutation(UpdateAttachment);
-
-  const doUpdateAttachment = useCallback(
-    (values) => {
-      const attachment = {
-        FileExtension: fileType,
-        Title: values.title,
-        Description: values.description
-      };
-
-      if (!isEmpty(row)) {
-        mutationUpdateAttachment({
-          variables: {
-            id: quoteId,
-            attachmentId: row['AttachmentId'],
-            attachment: attachment
-          }
-        });
-      }
-    },
-    [row, quoteId, mutationUpdateAttachment, fileType]
-  );
-
-  useEffect(() => {
-    if (row && resultUpdateAttachment && resultUpdateAttachment.UpdateAttachment) {
-      if (resultUpdateAttachment.UpdateAttachment.Status === 'SUCCESS') {
-        doGetAttachmentUploadPermission();
-        dispatch(
-          openSnackbar({
-            open: true,
-            message: resultUpdateAttachment.UpdateAttachment.Message,
-            variant: 'alert',
-            alert: {
-              color: 'success'
-            },
-            close: false
-          })
-        );
-      }
-      resetUpdateAttachment();
-    }
-  }, [row, resultUpdateAttachment, resetUpdateAttachment, dispatch, doGetAttachmentUploadPermission]);
-
-  const handleFileChange = (e) => {
-    if (e.target.files.length) {
-      setUploading(true);
-      var files = e.target.files;
-      setFileType(files[0].type.split('/').pop());
-    }
-  };
-
-  /* Formik submit */
-  const formikSubmit = (values) => {
-    setMutationStatus('');
-    setMutationMessage('');
-    doUpdateAttachment(values);
-  };
-
   /* Render */
   return (
     <>
@@ -150,13 +52,16 @@ const AttachmentModalEdit = ({ quoteId, row, callback }) => {
         enableReinitialize
         validationSchema={formikValidationSchema}
         onSubmit={async (values) => {
-          formikSubmit(values);
+          // Your form submit logic can be added here if needed
+          callback(true);
+          console.log(values);
         }}
       >
         {({ values, handleChange, handleBlur, isSubmitting }) => (
           <Form>
             <Grid container spacing={2} alignItems="center">
-              {mutationStatus === 'FAILED' && <Alert severity="error">{mutationMessage}</Alert>}
+              {/* Optional: Show a message based on mutationStatus and mutationMessage */}
+              {/* {mutationStatus === 'FAILED' && <Alert severity="error">{mutationMessage}</Alert>} */}
             </Grid>
             <Grid container columnSpacing={3} alignItems="center" mb={4} style={{ display: 'block' }}>
               <Grid item sm={12} xs={12} md={12} style={{ width: '100%', maxWidth: '100%' }}>
@@ -188,7 +93,8 @@ const AttachmentModalEdit = ({ quoteId, row, callback }) => {
             </Grid>
             <Grid container columnSpacing={3} alignItems="center" mb={4}>
               <Grid item sm={12}>
-                <input type="file" id="quote-attachment-file" disabled={uploading} onChange={handleFileChange} />
+                {/* Optional: Add file input for uploading */}
+                {/* <input type="file" id="quote-attachment-file" disabled={uploading} onChange={handleFileChange} /> */}
               </Grid>
               <Grid item xs={12}>
                 {files ? (
